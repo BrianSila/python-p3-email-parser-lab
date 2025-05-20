fc9debb709b5c9d738bf9c2579b225cfa312ae84
Brian <brian.sila1@student.moringaschool.com>
2025-05-20 09:34:51 +0300

    Email verification

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class EmailAddressParser():
+
+    def __init__(self, emails):
+        self.emails = emails
+
+    def parse(self):
+        emails = self.emails.replace(',', ' ').split()
+        
+        valid_emails = [
+            email for email in emails 
+            if '@' in email and 
+               '.' in email and
+               email.count('@') == 1 and
+               email.index('@') < email.rindex('.')
+        ]
+        return sorted(list(set(valid_emails)))
